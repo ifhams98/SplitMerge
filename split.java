931cafbb859b6e3022f380d3d4cdbc16adfb01f3
Ifham Shahid <51059216+ifhams98@users.noreply.github.com>
2020-06-28 20:31:49 +0500

    Run this to split files and store them in your PC

---
+++ split.java
@@ -0,0 +1,36 @@
+package test1;
+import java.io.*;
+
+/**
+ *
+ * @author Ifham Shahid
+ */
+public class split {
+    public static void splitFile(File f) throws IOException {
+        int partCounter = 1;//I like to name parts from 001, 002, 003, ...
+                            //you can change it to 0 if you want 000, 001, ...
+
+        int sizeOfFiles = 250 * 1024;// 1MB
+        byte[] buffer = new byte[sizeOfFiles];
+
+        String fileName = f.getName();
+
+        //try-with-resources to ensure closing stream
+        try (FileInputStream fis = new FileInputStream(f);
+             BufferedInputStream bis = new BufferedInputStream(fis)) {
+
+            int bytesAmount = 0;
+            while ((bytesAmount = bis.read(buffer)) > 0) {
+                //write each chunk of data into separate file with different number in name
+                String filePartName = String.format("%s.%03d", fileName, partCounter++);
+                File newFile = new File(f.getParent(), filePartName);
+                try (FileOutputStream out = new FileOutputStream(newFile)) {
+                    out.write(buffer, 0, bytesAmount);
+                }
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+    splitFile(new File("E:\\test.JPG")); //give path of the file to split
+    }
+}
